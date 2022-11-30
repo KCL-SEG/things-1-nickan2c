4bc75afd9f0179ce61c6255b79f83c0a7f747611
Nickan Tousi <nickan.tousi@kcl.ac.uk>
2022-11-30 12:12:14 +0000

    Exercise 1

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from thingsApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', 'thingsApp.views.index', name='index'),
+    path('', views.index, name='index'),
 ]
