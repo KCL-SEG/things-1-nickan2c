d7ae4d56e255aa54e163b14ccf66e6f6514bcc71
Nickan Tousi <nickan.tousi@kcl.ac.uk>
2022-11-30 12:16:19 +0000

    Exercise 2 and 3

--- thingsApp/models.py
+++ thingsApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Thing(models.Model):
+    # name must be unique, must not be blank, and must consist of 30 characters or less.
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    # description must not be blank, and must consist of 100 characters or less.
+    description = models.CharField(max_length=100, blank=False)
+    quanitity = models.IntegerField(min_value=0, max_value=100)
