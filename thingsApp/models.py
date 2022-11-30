3a7897d8b43296544570c0b36bb7d30855ee7500
Nickan Tousi <nickan.tousi@kcl.ac.uk>
2022-11-30 12:42:49 +0000

    rename tests

--- thingsApp/models.py
+++ thingsApp/models.py
@@ -5,5 +5,5 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=120 , blank=True)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
